0ca96e0e58c24669b88ffb2fc5642c602d2555c5
Arakene <arakene_dev@naver.com>
2021-07-15 23:01:54 +0900

    Update Skill

--- app/src/main/java/arakene/lastoriginsimulator/bioroid/Skill.kt
+++ app/src/main/java/arakene/lastoriginsimulator/bioroid/Skill.kt
@@ -5,45 +5,22 @@ open class Skill {
     companion object {
         const val APPLY = 1
         const val NONE = 0
-        const val PASSIVE1 = "passive1"
-        const val PASSIVE2 = "passive2"
-        const val PASSIVE3 = "passive3"
     }
 
-    var passive1 = arrayOf(
+    var skillRange = arrayOf(
         IntArray(3),
         IntArray(3),
         IntArray(3)
     )
 
-    var passive2 = arrayOf(
-        IntArray(3),
-        IntArray(3),
-        IntArray(3)
-    )
-
-    var passive3 = arrayOf(
-        IntArray(3),
-        IntArray(3),
-        IntArray(3)
-    )
-
-    var passiveEffect = HashMap<String, HashMap<String, Double>>()
+    var skillEffect = HashMap<String, Double>()
 
     fun applyPassive(targets: Array<Bioroid>) {
         for (target in targets) {
-            if (isPosition(target, passive1)) {
-                val effects = passiveEffect[PASSIVE1]!!
-                for (key in effects.keys) {
-                    val targetStat = target.stats[key]
-                    target.stats[key] = targetStat!! * effects[key]!!
+            if (isPosition(target, skillRange)) {
+                for (stat in skillEffect.keys) {
+                    target.stats[stat] = target.stats[stat]!! * skillEffect[stat]!!
                 }
-            }
-            if (isPosition(target, passive2)) {
-
-            }
-            if (isPosition(target, passive3)) {
-
             }
         }
     }
