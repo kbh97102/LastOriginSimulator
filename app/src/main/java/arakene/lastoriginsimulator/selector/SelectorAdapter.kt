90ca5daed26b85759bed0cc2e7f1cac9f536458f
Arakene <arakane_dev@naver.com>
2021-07-17 14:41:40 +0900

    Update adapter

--- app/src/main/java/arakene/lastoriginsimulator/selector/SelectorAdapter.kt
+++ app/src/main/java/arakene/lastoriginsimulator/selector/SelectorAdapter.kt
@@ -1,18 +1,22 @@
 package arakene.lastoriginsimulator.selector
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import arakene.lastoriginsimulator.R
 import arakene.lastoriginsimulator.bioroid.Bioroid
 
 class SelectorAdapter : RecyclerView.Adapter<SelectorViewHolder>() {
     var datas = ArrayList<Bioroid>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SelectorViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+        return SelectorViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SelectorViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int = datas.size
