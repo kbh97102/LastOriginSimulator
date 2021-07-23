3ff85601e246199b8204a887b95708c2bbd48c8c
Arakene <arakene_dev@naver.com>
2021-07-24 07:56:14 +0900

    Add item view

---
+++ app/src/main/java/arakene/lastoriginsimulator/item/ItemAdapter.kt
@@ -0,0 +1,20 @@
+package arakene.lastoriginsimulator.item
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter : RecyclerView.Adapter<ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/arakene/lastoriginsimulator/item/ItemViewHolder.kt
@@ -0,0 +1,7 @@
+package arakene.lastoriginsimulator.item
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
